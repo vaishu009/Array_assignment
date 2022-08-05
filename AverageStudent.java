a0672b4b10752ae2337d1615b8367f8e8161476a
Vaishnavi Kane <109944617+vaishu009@users.noreply.github.com>
2022-08-05 11:26:04 +0530

    Write a program to print the roll number and average marks of 8 student

--- AverageStudent.java
+++ AverageStudent.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,8 +13,33 @@
  */
 class AverageStudent {
 
-    void readdata() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+   public static void main(String[] args) {
+    AverageStudent a1 = new AverageStudent();
+    a1.readdata();
+    
+}
+    
+  public static void readdata (){  
+int roll, s1, s2, s3;
+Scanner input = new Scanner(System.in);
+for(int i=0; i<=8; i++){
+
+for(int j=i; j<=8; j++){
+System.out.print("Enter Roll number :");
+roll = input.nextInt();
+
+System.out.print("Enter First subject number :");
+s1 = input.nextInt();
+
+System.out.print("Enter second subject number :");
+s2 = input.nextInt();
+
+System.out.print("Enter third subject number :");
+s3 = input.nextInt();
+
+int avg = (s1 +s2 + s3)/3 ;
+System.out.println("Student of Roll No " +roll + " get Avarage mark of "+avg);
+}
+}
     
 }
